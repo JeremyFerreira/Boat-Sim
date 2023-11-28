ed63e2070b93769e5d536cb7a7e6eadb3c6ddabe
jeremyferreira <Jeremyblazerpro@gmail.com>
2023-11-28 22:21:17 +0100

    Object Buoyancy (floating physics)

---
+++ Assets/Scripts/Extention classes/MathExtension.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathExtension
+{
+    public static float TAU = 6.2831855f;
+    public static Vector3 RotateVectorOnXZPlane(Vector3 vectorToRotate, float angleInRad)
+    {
+        return new Vector3(vectorToRotate.x * Mathf.Cos(angleInRad) - vectorToRotate.z * Mathf.Sin(angleInRad), 0f, vectorToRotate.z * Mathf.Cos(angleInRad) + vectorToRotate.x * Mathf.Sin(angleInRad)).normalized * vectorToRotate.magnitude;
+    }
+    public static Vector3 RotateVectorOnXYPlane(Vector3 vectorToRotate, float angle)
+    {
+        return new Vector3(vectorToRotate.x * Mathf.Cos(angle) - vectorToRotate.y * Mathf.Sin(angle), vectorToRotate.y * Mathf.Cos(angle) + vectorToRotate.x * Mathf.Sin(angle),0f).normalized * vectorToRotate.magnitude;
+    }
+    public static float AngleBetweenVectorsInDeg(Vector3 vector1, Vector3 vector2)
+    {
+        //The angle between two vectors a and b is found with the formula angle = cos-1 [ (a. b) / (|a| |b|) ]
+        float angle = Mathf.Acos(Vector3.Dot(vector1,vector2)/(vector1.magnitude*vector2.magnitude));
+        return angle*Mathf.Rad2Deg;
+    }
+    public static float AngleBetweenVectors(Vector3 vector1, Vector3 vector2)
+    {
+        //The angle between two vectors a and b is found with the formula angle = cos-1 [ (a. b) / (|a| |b|) ]
+        float angle = Mathf.Acos(Vector3.Dot(vector1, vector2) / (vector1.magnitude * vector2.magnitude));
+        return angle;
+    }
+    public static Vector3 FlatVectorXZ(Vector3 vector)
+    {
+        return new Vector3(vector.x, 0,vector.z);
+    }
+    public static Vector3 FlatVectorXY(Vector3 vector)
+    {
+        return new Vector3(vector.x,vector.y, 0f);
+    }
+    public static Vector3 GetPositionAtDistanceOfPoint(Vector3 point, Vector3 destinationPoint, float distance) 
+    {
+        Vector3 direction = (destinationPoint - point).normalized;
+        return destinationPoint - direction * distance;
+    }
+}
---
+++ Assets/Scripts/Physic/FloatingObject.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FloatingObject : MonoBehaviour
+{
+    [SerializeField] private WaterElevation _waterElevation;
+    [SerializeField] private Transform[] _floatingPoints;
+    [SerializeField] private float _floatingForce;
+    [SerializeField] private float _waterLinearDrag;
+    [SerializeField] private float _airLinearDrag;
+    [SerializeField] private float _waterAngularDrag;
+    [SerializeField] private float _airAngularDrag;
+    private Rigidbody _objectRigidBody;
+    [SerializeField] private float _averageHeight;
+
+    private void OnValidate()
+    {
+        _objectRigidBody = GetComponent<Rigidbody>();
+        _waterElevation = FindAnyObjectByType<WaterElevation>();
+
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _objectRigidBody = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (Transform floatingPoint in _floatingPoints)
+        {
+            float heightBetweenPointAndWater = -(floatingPoint.transform.position.y - _waterElevation.GetElevation(floatingPoint.transform.position.x, floatingPoint.transform.position.z));
+            if (heightBetweenPointAndWater > 0)
+            {
+                _objectRigidBody.AddForceAtPosition(Vector3.up * Mathf.Sqrt(heightBetweenPointAndWater) * _floatingForce / _floatingPoints.Length, floatingPoint.transform.position, ForceMode.Acceleration);
+            }
+            
+        }
+        //calculate Drag
+        float ratioMassUnderwater = GetRatioMassUnderWater();
+        float ratioMassInAir = 1-ratioMassUnderwater;
+        _objectRigidBody.drag = ratioMassUnderwater * _waterLinearDrag + ratioMassInAir * _airLinearDrag;
+        _objectRigidBody.angularDrag =  ratioMassUnderwater * _waterAngularDrag + ratioMassInAir * _airAngularDrag;
+    }
+    //Big Approximation!
+    float GetRatioMassUnderWater()
+    {
+        float centerOfmassDisatnceToWaterSurface = _waterElevation.GetElevation(transform.position.x, transform.position.z) - _objectRigidBody.worldCenterOfMass.y;
+        float halfHeight = _averageHeight / 2f;
+        centerOfmassDisatnceToWaterSurface = Mathf.Clamp(centerOfmassDisatnceToWaterSurface, -halfHeight, halfHeight);
+        
+        return Mathf.Lerp(0,1, ((centerOfmassDisatnceToWaterSurface/halfHeight)+1)/2f);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        foreach (Transform floatingPoint in _floatingPoints)
+        {
+            float heightBetweenPointAndWater = -(floatingPoint.transform.position.y - _waterElevation.GetElevation(floatingPoint.transform.position.x, floatingPoint.transform.position.z));
+            if (heightBetweenPointAndWater > 0)
+            {
+                heightBetweenPointAndWater = Mathf.Sqrt(heightBetweenPointAndWater);
+            }
+            else
+            {
+                heightBetweenPointAndWater = -Mathf.Sqrt(-heightBetweenPointAndWater);
+            }
+            Vector3 directionForce = _waterElevation.GetNormal(floatingPoint.transform.position.x, floatingPoint.transform.position.z) * heightBetweenPointAndWater;
+            Gizmos.DrawLine(floatingPoint.transform.position, floatingPoint.transform.position + directionForce*5);
+        }
+        Handles.Label(transform.position+Vector3.up,GetRatioMassUnderWater().ToString());
+    }
+}
