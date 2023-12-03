08839cd8c0a0dc526b94545ba1e54ab5050d1ce0
jeremyferreira <Jeremyblazerpro@gmail.com>
2023-12-03 16:46:53 +0100

    open level

--- Assets/Scripts/Hub/BoatSelector.cs
+++ Assets/Scripts/Hub/BoatSelector.cs
@@ -2,29 +2,56 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BoatSelector : MonoBehaviour
 {
     [SerializeField] CinemachineBrain _brain;
     [SerializeField] CinemachineVirtualCamera[] _virtualCameras;
+    [SerializeField] CinemachineVirtualCamera[] _boatVirtualCameras;
     int _currentCameraIndex;
     CinemachineVirtualCamera _currentCamera;
+    [SerializeField] GameObject[] _buttonsNavigate;
     private void Awake()
     {
         foreach (var cam in _virtualCameras)
         {
             cam.gameObject.SetActive(false);
         }
+        foreach (var button in _buttonsNavigate)
+        {
+            button.SetActive(false);
+        }
+        _buttonsNavigate[0].SetActive(true);
         _currentCameraIndex = 0;
         _currentCamera = _virtualCameras[_currentCameraIndex];
         _currentCamera.gameObject.SetActive(true);
     }
     public void SwitchCamera(bool next)
     {
+        _buttonsNavigate[_currentCameraIndex].SetActive(false);
         _currentCameraIndex += next ? 1 : _virtualCameras.Length - 1;
         _currentCameraIndex %= _virtualCameras.Length;
         _currentCamera.gameObject.SetActive(false);
         _currentCamera = _virtualCameras[_currentCameraIndex];
         _currentCamera.gameObject.SetActive(true);
+        _buttonsNavigate[_currentCameraIndex].SetActive(true);
+
+    }
+    public void OpenScene(int index)
+    {
+        _currentCamera?.gameObject.SetActive(false);
+        _boatVirtualCameras[_currentCameraIndex].gameObject.SetActive(true);
+
+        StartCoroutine(WaitForEndOfBlend(index));
+    }
+    IEnumerator WaitForEndOfBlend(int index)
+    {
+        yield return new WaitForSeconds(0.5f);
+        while(_brain.IsBlending)
+        {
+            yield return null;
+        }
+        SceneManager.LoadScene(index);
     }
 }
