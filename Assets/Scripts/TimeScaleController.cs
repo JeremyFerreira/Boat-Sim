33c927f274971452e9c52a78421e9678b109a086
KaprisDimitri <kaprisdimitri@gmail.com>
2023-12-21 16:24:43 +0100

    Pause Game

--- Assets/Scripts/TimeScaleController.cs
+++ Assets/Scripts/TimeScaleController.cs
@@ -5,4 +5,14 @@ using UnityEngine;
 public class TimeScaleController : MonoBehaviour
 {
   //  private void StartTime
+
+    public void Pausegame ()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void PlayGame ()
+    {
+        Time.timeScale = 1;
+    }
 }
